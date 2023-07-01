ca0f518bfd4748a1ad1be7a2521520cfae355818
Vito <92116141+Utds-Group@users.noreply.github.com>
2023-07-01 22:33:11 +0200

    Update kontin.js

--- assets/js/kontin.js
+++ assets/js/kontin.js
@@ -266,23 +266,24 @@
           required: true,
         },
       },
-      submitHandler: function (form) {
-        // sending value with ajax request
-        $.post(
-          $(form).attr("action"),
-          $(form).serialize(),
-          function (response) {
-            $(form).parent().find(".result").append(response);
-            $(form).find('input[type="text"]').val("");
-            $(form).find('input[type="email"]').val("");
-            $(form).find("textarea").val("");
-          }
-        );
-        return false;
-      },
+      jQuery(document).ready(function(){
+	jQuery("form").submit(function() { // Событие отправки с формы
+		var form_data = jQuery(this).serialize(); // Собираем данные из полей
+		jQuery.ajax({
+			type: "POST", // Метод отправки
+			url: "sendform.php", // Путь к PHP обработчику sendform.php
+			data: form_data,
+			success: swal({
+				title: "Спасибо за заявку!",
+                type: "success",
+                showConfirmButton: false,
+                timer: 2000
+            })
+        });
+        $(this).find('input, textarea').prop('disabled', true);
+        event.preventDefault();
     });
-  }
-
+});
   // mailchimp form
   if ($(".mc-form").length) {
     $(".mc-form").each(function () {
